8158f9a0ae8a191ec681cd889344b1d74f57d23a
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-22 07:22:14 +0100

    Consolidate notebook writing to sessionstart hook alone, and allow to specify an optional work directory

--- nbvalx/pytest_hooks_notebooks.py
+++ nbvalx/pytest_hooks_notebooks.py
@@ -47,21 +47,29 @@ else:
             "Please do not start pytest under mpirun. Use the --np pytest option.")
         # Tag actions
         parser.addoption(
-            "--tag-action", type=str, default="collect-notebooks", help="action on notebooks with tags")
-        parser.addoption("--tag-collapse", action="store_true", help="collapse notebook to active tag")
+            "--tag-action", type=str, default="collect-notebooks", help="Action on notebooks with tags")
+        parser.addoption("--tag-collapse", action="store_true", help="Collapse notebook to active tag")
+        # Working directory
+        parser.addoption(
+            "--work-dir", type=str, default="", help="Working directory in which to run the tests")
 
     def sessionstart(session: _pytest.main.Session) -> None:
         """Parameterize jupyter notebooks based on available tags."""
+        # Verify that nbval is not explicitly provided on the command line
+        nbval = session.config.option.nbval
+        assert not nbval, "--nbval is implicitly enabled, do not provide it on the command line"
+        # Verify parallel options
+        np = session.config.option.np
+        assert np > 0
+        # Verify tag options
         tag_action = session.config.option.tag_action
         tag_collapse = session.config.option.tag_collapse
         assert tag_action in ("create-notebooks", "collect-notebooks")
-        # Patch default norecursepatterns so that the files we create will not get ignored
-        norecursepatterns = session.config.getini("norecursedirs")
-        assert ".*" in norecursepatterns
-        norecursepatterns.remove(".*")
-        norecursepatterns.append(".ipynb_checkpoints")
-        norecursepatterns.append(".ipynb_mpi")
-        norecursepatterns.append(".ipynb_tags")
+        # Verify working directory options
+        if session.config.option.work_dir == "":
+            session.config.option.work_dir = f".ipynb_pytest/np_{np}/collapse_{tag_collapse}"
+        work_dir = session.config.option.work_dir
+        assert work_dir not in ("", ".")
         # Create tags based on existing notebooks
         dirpaths = list()
         for arg in session.config.args:
@@ -77,8 +85,7 @@ else:
                 assert os.path.isfile(dirpath)
                 files.append(dirpath)
             for filepath in files:
-                if fnmatch.fnmatch(filepath, "**/*.ipynb") and not fnmatch.fnmatch(
-                        filepath, "**/.ipynb_tags/*.ipynb"):
+                if fnmatch.fnmatch(filepath, "**/*.ipynb"):
                     # Read in notebook
                     with open(filepath) as f:
                         nb = nbformat.read(f, as_version=4)
@@ -98,120 +105,123 @@ else:
                                     "Use a standalone cell for %register_run_if_allowed_tags")
                                 line = lines[0].replace("%register_run_if_allowed_tags ", "")
                                 allowed_tags = [tag.strip() for tag in line.split(",")]
-                    if not run_if_loaded or len(allowed_tags) == 0:
-                        continue
                     # Create temporary copies for each allowed tag
-                    for tag in allowed_tags:
-                        # Determine what will be the new notebook name and path
-                        tags_dir = os.path.join(
-                            os.path.dirname(filepath), ".ipynb_tags", f"collapse_{tag_collapse}")
-                        ipynb_path = os.path.join(
-                            tags_dir, os.path.basename(filepath).replace(".ipynb", f"[{tag}].ipynb"))
-                        # Replace tag and, if collapsing notebooks, strip cells with other tags
-                        cells_tag = list()
-                        for cell in nb.cells:
-                            if cell.cell_type == "code":
-                                if (cell.source.startswith("%load_ext nbvalx")
-                                        or cell.source.startswith("%register_run_if_allowed_tags")):
-                                    if not tag_collapse:
-                                        cells_tag.append(cell)
-                                elif cell.source.startswith("%register_run_if_current_tag"):
-                                    assert len(cell.source.splitlines()) == 1, (
-                                        "Use a standalone cell for %register_run_if_current_tag")
-                                    if not tag_collapse:
-                                        cell_tag = copy.deepcopy(cell)
-                                        cell_tag.source = f"%register_run_if_current_tag {tag}"
-                                        cells_tag.append(cell_tag)
-                                elif "%%run_if" in cell.source:
-                                    if tag_collapse:
-                                        lines = cell.source.splitlines()
-                                        magic_line_index = 0
-                                        while not lines[magic_line_index].startswith("%%run_if"):
-                                            magic_line_index += 1
-                                        assert magic_line_index < len(lines)
-                                        line = lines[magic_line_index].replace("%%run_if ", "")
-                                        allowed_tags = [tag.strip() for tag in line.split(",")]
-                                        if tag in allowed_tags:
-                                            cell_tag = copy.deepcopy(cell)
-                                            lines.remove(lines[magic_line_index])
-                                            cell_tag.source = "\n".join(lines)
+                    nb_tags = dict()
+                    if run_if_loaded and len(allowed_tags) > 0:
+                        for tag in allowed_tags:
+                            # Determine what will be the new notebook path
+                            ipynb_path = os.path.join(
+                                os.path.dirname(filepath), work_dir,
+                                os.path.basename(filepath).replace(".ipynb", f"[{tag}].ipynb"))
+                            # Replace tag and, if collapsing notebooks, strip cells with other tags
+                            cells_tag = list()
+                            for cell in nb.cells:
+                                cell_tag = copy.deepcopy(cell)
+                                if cell.cell_type == "code":
+                                    if (cell.source.startswith("%load_ext nbvalx")
+                                            or cell.source.startswith("%register_run_if_allowed_tags")):
+                                        if not tag_collapse:
                                             cells_tag.append(cell_tag)
+                                    elif cell.source.startswith("%register_run_if_current_tag"):
+                                        assert len(cell.source.splitlines()) == 1, (
+                                            "Use a standalone cell for %register_run_if_current_tag")
+                                        if not tag_collapse:
+                                            cell_tag.source = f"%register_run_if_current_tag {tag}"
+                                            cells_tag.append(cell_tag)
+                                    elif "%%run_if" in cell.source:
+                                        if tag_collapse:
+                                            lines = cell.source.splitlines()
+                                            magic_line_index = 0
+                                            while not lines[magic_line_index].startswith("%%run_if"):
+                                                magic_line_index += 1
+                                            assert magic_line_index < len(lines)
+                                            line = lines[magic_line_index].replace("%%run_if ", "")
+                                            allowed_tags = [tag.strip() for tag in line.split(",")]
+                                            if tag in allowed_tags:
+                                                lines.remove(lines[magic_line_index])
+                                                cell_tag.source = "\n".join(lines)
+                                                cells_tag.append(cell_tag)
+                                        else:
+                                            cells_tag.append(cell_tag)
+                                    elif cell.source.startswith("__notebook_name__"):
+                                        assert len(cell.source.splitlines()) == 1, (
+                                            "Use a standalone cell for __notebook_name__")
+                                        if ".ipynb_pytest" in work_dir:
+                                            notebook_name_tag = os.path.relpath(
+                                                ipynb_path, os.path.join(os.path.dirname(filepath), ".ipynb_pytest"))
+                                        else:  # pragma: no cover
+                                            notebook_name_tag = os.path.relpath(
+                                                ipynb_path, os.path.join(os.path.dirname(filepath), work_dir))
+                                        cell_tag.source = f'__notebook_name__ = "{notebook_name_tag}"'
+                                        cells_tag.append(cell_tag)
                                     else:
-                                        cells_tag.append(cell)
-                                elif cell.source.startswith("__notebook_name__"):
-                                    assert len(cell.source.splitlines()) == 1, (
-                                        "Use a standalone cell for __notebook_name__")
-                                    cell_tag = copy.deepcopy(cell)
-                                    notebook_name_tag = os.path.relpath(
-                                        ipynb_path, os.path.join(os.path.dirname(filepath), ".ipynb_tags"))
-                                    cell_tag.source = f'__notebook_name__ = "{notebook_name_tag}"'
-                                    cells_tag.append(cell_tag)
+                                        cells_tag.append(cell_tag)
                                 else:
-                                    cells_tag.append(cell)
-                            else:
-                                cells_tag.append(cell)
-                        # Attach cells to a copy of the notebook
-                        nb_tag = copy.deepcopy(nb)
-                        nb_tag.cells = cells_tag
-                        # Write modified notebook to a temporary file
-                        os.makedirs(tags_dir, exist_ok=True)
-                        with open(ipynb_path, "w") as f:
-                            nbformat.write(nb_tag, str(ipynb_path))
-            # Allow collecting the create copies, but only for the current value of tag_collapse
-            norecursepatterns.remove(".ipynb_tags")
-            norecursepatterns.append(os.path.join(".ipynb_tags", f"collapse_{not tag_collapse}"))
+                                    cells_tag.append(cell_tag)
+                            # Attach cells to a copy of the notebook
+                            nb_tag = copy.deepcopy(nb)
+                            nb_tag.cells = cells_tag
+                            # Store notebook in dictionary
+                            nb_tags[ipynb_path] = nb_tag
+                    else:
+                        # Determine what will be the new notebook path
+                        ipynb_path = os.path.join(
+                            os.path.dirname(filepath), work_dir, os.path.basename(filepath))
+                        # Store notebook in dictionary
+                        nb_tags[ipynb_path] = nb
+                    # Add parallel support
+                    if np > 1:
+                        for (ipynb_path, nb_tag) in nb_tags.items():
+                            # Add the %%px magic to every existing cell
+                            for cell in nb_tag.cells:
+                                if cell.cell_type == "code":
+                                    cell.source = "%%px\n" + cell.source
+                            # Add a cell on top to start a new ipyparallel cluster
+                            cluster_start_code = f"""import ipyparallel as ipp
 
-    def collect_file(path: py.path.local, parent: _pytest.nodes.Collector) -> nbval.plugin.IPyNbFile:
-        """Collect IPython notebooks using a custom pytest nbval hook."""
-        opt = parent.config.option
-        assert not opt.nbval, "--nbval is implicitly enabled, do not provide it on the command line"
-        if path.fnmatch("**/*.ipynb"):
-            if opt.np > 1:
-                # Read in notebook
-                with open(path) as f:
-                    nb = nbformat.read(f, as_version=4)
-                # Add the %%px magic to every existing cell
-                for cell in nb.cells:
-                    if cell.cell_type == "code":
-                        cell.source = "%%px\n" + cell.source
-                # Add a cell on top to start a new ipyparallel cluster
-                cluster_start_code = f"""import ipyparallel as ipp
-
-cluster = ipp.Cluster(engines="MPI", profile="mpi", n={opt.np})
+cluster = ipp.Cluster(engines="MPI", profile="mpi", n={np})
 cluster.start_and_connect_sync()"""
-                cluster_start_cell = nbformat.v4.new_code_cell(cluster_start_code)
-                cluster_start_cell.id = "cluster_start"
-                nb.cells.insert(0, cluster_start_cell)
-                # Add a further cell on top to disable garbage collection
-                gc_disable_code = """%%px
+                            cluster_start_cell = nbformat.v4.new_code_cell(cluster_start_code)
+                            cluster_start_cell.id = "cluster_start"
+                            nb_tag.cells.insert(0, cluster_start_cell)
+                            # Add a further cell on top to disable garbage collection
+                            gc_disable_code = """%%px
 import gc
 
 gc.disable()"""
-                gc_disable_cell = nbformat.v4.new_code_cell(gc_disable_code)
-                gc_disable_cell.id = "gc_disable"
-                nb.cells.insert(1, gc_disable_cell)
-                # Add a cell at the end to re-enable garbage collection
-                gc_enable_code = """%%px
+                            gc_disable_cell = nbformat.v4.new_code_cell(gc_disable_code)
+                            gc_disable_cell.id = "gc_disable"
+                            nb_tag.cells.insert(1, gc_disable_cell)
+                            # Add a cell at the end to re-enable garbage collection
+                            gc_enable_code = """%%px
 gc.enable()
 gc.collect()"""
-                gc_enable_cell = nbformat.v4.new_code_cell(gc_enable_code)
-                gc_enable_cell.id = "gc_enable"
-                nb.cells.append(gc_enable_cell)
-                # Add a cell at the end to stop the ipyparallel cluster
-                cluster_stop_code = """cluster.stop_cluster_sync()"""
-                cluster_stop_cell = nbformat.v4.new_code_cell(cluster_stop_code)
-                cluster_stop_cell.id = "cluster_stop"
-                nb.cells.append(cluster_stop_cell)
-                # Write modified notebook to a temporary file
-                mpi_dir = os.path.join(path.dirname, ".ipynb_mpi")
-                os.makedirs(mpi_dir, exist_ok=True)
-                ipynb_path = path.new(dirname=mpi_dir)
-                with open(ipynb_path, "w") as f:
-                    nbformat.write(nb, str(ipynb_path))
-            else:  # pragma: no cover
-                ipynb_path = path
-            if opt.tag_action != "create-notebooks":
-                return nbval.plugin.IPyNbFile.from_parent(parent, fspath=ipynb_path)
+                            gc_enable_cell = nbformat.v4.new_code_cell(gc_enable_code)
+                            gc_enable_cell.id = "gc_enable"
+                            nb_tag.cells.append(gc_enable_cell)
+                            # Add a cell at the end to stop the ipyparallel cluster
+                            cluster_stop_code = """cluster.stop_cluster_sync()"""
+                            cluster_stop_cell = nbformat.v4.new_code_cell(cluster_stop_code)
+                            cluster_stop_cell.id = "cluster_stop"
+                            nb_tag.cells.append(cluster_stop_cell)
+                    # Write modified notebooks to the work directory
+                    for (ipynb_path, nb_tag) in nb_tags.items():
+                        os.makedirs(os.path.dirname(ipynb_path), exist_ok=True)
+                        with open(ipynb_path, "w") as f:
+                            nbformat.write(nb_tag, str(ipynb_path))
+            # If the work directory is hidden, patch default norecursepatterns so that the files
+            # we created will not get ignored
+            if work_dir.startswith("."):
+                norecursepatterns = session.config.getini("norecursedirs")
+                assert ".*" in norecursepatterns
+                norecursepatterns.remove(".*")
+
+    def collect_file(path: py.path.local, parent: _pytest.nodes.Collector) -> nbval.plugin.IPyNbFile:
+        """Collect IPython notebooks using the custom pytest nbval collector."""
+        tag_action = parent.config.option.tag_action
+        work_dir = parent.config.option.work_dir
+        if path.fnmatch(f"{work_dir}/*.ipynb") and tag_action != "create-notebooks":
+            return nbval.plugin.IPyNbFile.from_parent(parent, fspath=path)
 
     def runtest_setup(item: _pytest.nodes.Item) -> None:
         """Insert skips on cell failure."""
