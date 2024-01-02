ac08a36dd72c3a78081f8b7fa07cd10a061b816c
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-02 20:28:48 +0100

    Add test for coverage and local data files

---
+++ tests/notebooks/data/coverage/coverage_mock_module.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2022-2024 by the nbvalx authors
+#
+# This file is part of nbvalx.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+"""A mock module to test coverage integration."""
+
+def my_sum(a: int, b: int) -> int:
+    """Sum two numbers."""
+    return a + b
+
+
+def pragma_ignored() -> None:  # pragma: no cover
+    """Ignored function that will be considered as covered even though it is untested."""
+    pass
