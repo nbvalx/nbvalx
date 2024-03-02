a1773092cf45a8fba199c92f418676433fc3dd4f
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-03-02 17:11:40 +0100

    Remove mypy ignore which seems to be unnecessary with newer ipython versions

--- nbvalx/jupyter_magics.py
+++ nbvalx/jupyter_magics.py
@@ -154,9 +154,9 @@ def unload_ipython_extension(
     ipython: IPython.core.interactiveshell.InteractiveShell
 ) -> None:
     """Unregister the magics defined in this module when the extension unloads."""
-    del ipython.magics_manager.magics["cell"]["register_run_if_allowed_tags"]  # type: ignore[union-attr]
-    del ipython.magics_manager.magics["cell"]["register_run_if_current_tags"]  # type: ignore[union-attr]
-    del ipython.magics_manager.magics["cell"]["run_if"]  # type: ignore[union-attr]
+    del ipython.magics_manager.magics["cell"]["register_run_if_allowed_tags"]
+    del ipython.magics_manager.magics["cell"]["register_run_if_current_tags"]
+    del ipython.magics_manager.magics["cell"]["run_if"]
     IPythonExtension.loaded = False
     IPythonExtension.allowed_tags = {}
     IPythonExtension.current_tags = {}
